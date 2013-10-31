ce59048be41280f5b20ec90842c76b70ba00abcf
Robert Pickering <robert@strangelights.com>
2013-10-31 16:29:20 +0000

    add ended event to player

--- src/Undertone/Player.fs
+++ src/Undertone/Player.fs
@@ -2,10 +2,12 @@
 // The IPlayer interface that allows client code to play a sound wave
 //////////////////////////////////////////////////////////////////////////////
 namespace Undertone
+open System
 
 type IPlayer =
     abstract Play: Unit -> Unit
     abstract Stop: Unit -> Unit
     abstract Repeat: bool with get, set
     abstract SetSampleSource: seq<float> -> unit
+    abstract Ended: IEvent<EventHandler, EventArgs>
 
--- src/Undertone/Player.net.fs
+++ src/Undertone/Player.net.fs
@@ -2,14 +2,17 @@
 // Implementation IPlayer interface for .NET
 //////////////////////////////////////////////////////////////////////////////
 namespace Undertone
+open System
 open Undertone
 open NAudio.Wave
 
-type NetPlayer(sampleSource) =
+type NetPlayer(sampleSource) as x =
     let sampleSource = ref sampleSource
     let repeat = ref false
     let doingStop = ref false
     let player = new WaveOut()
+    let ended = Event<EventHandler, EventArgs>()
+
     do player.Init(new NAudioWaveStreamSource(!sampleSource))
     let doRepeat() =
         if !repeat && not !doingStop then
@@ -17,12 +20,14 @@ type NetPlayer(sampleSource) =
             player.Play()
         if !doingStop then doingStop := false
     do player.PlaybackStopped.AddHandler(fun _ _ -> doRepeat())
+    do player.PlaybackStopped.AddHandler(fun _ _ -> if not !repeat then ended.Trigger(x, EventArgs.Empty))
     interface IPlayer with
         member __.Play() = player.Play()
         member __.Stop() =
             if player.PlaybackState = PlaybackState.Playing then 
                 doingStop := true
             player.Stop()
+            ended.Trigger(x, EventArgs.Empty)
         member __.Repeat
             with get() = !repeat
             and  set x = repeat := x
@@ -30,6 +35,7 @@ type NetPlayer(sampleSource) =
             // TODO this needs to handled better
             sampleSource := newSampleSource
             x.Stop()
+        member x.Ended = ended.Publish
 
     member x.Play() =
         let sp = x :> IPlayer
@@ -47,6 +53,9 @@ type NetPlayer(sampleSource) =
     member x.SetSampleSource sampleSource = 
         let sp = x :> IPlayer
         sp.SetSampleSource sampleSource
+    member x.Ended =
+        let sp = x :> IPlayer
+        sp.Ended
 
 type Player private() =
     static member Play sampleSource =
